266358a5d5b0967b2bff56f57affd647943e0706
Dennis Velez <velez.dennis0@outlook.com>
2018-08-05 20:35:47 -0400

    Add project files.

---
+++ EmployeeWeb/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EmployeeWeb.Models
+{
+    public class Employee
+    {
+
+        public int Id { get; set; }
+        public string Fullname { get; set; }
+        public string Department { get; set; }
+
+    }
+}
